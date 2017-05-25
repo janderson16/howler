fbce436f3a0bc61ffe94b3494d64ff612d5b5571
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-05-24 21:07:15 -0600

    Cews twitter (#2)
    * update post_api for running localhost
    
    * update routes file to include twitter auth and logout
    
    * add sessions controller
    
    * add twitter to gemfile
    
    * add user model with migration
    
    * add twitter_service for taking in authenticated user data; add current_user to application_controller
    
    * add omniauth file in initializers folder

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+   user = User.find_or_create_by(uid:auth["uid"])
+   user.uid             = auth["uid"]
+   user.nickname        = auth["info"]["nickname"]
+   user.name            = auth["info"]["name"]
+   user.description     = auth["info"]["description"]
+   user.image           = auth["info"]["image"]
+   user.token           = auth["credentials"]["token"]
+   user.save
+   user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root "home#show"
 
+  get '/auth/twitter', as: :login
+  get '/auth/twitter/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   namespace :api do
     namespace :v1 do
       resources :tones, only: [:create, :index]
