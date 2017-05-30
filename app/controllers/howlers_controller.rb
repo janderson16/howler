c754bf30c4ea74c7f6a90195453d625d079e07de
Spencer Carter <scmountain@gmail.com>
2017-05-30 17:07:21 -0600

    post merge

--- app/controllers/howlers_controller.rb
+++ app/controllers/howlers_controller.rb
@@ -2,4 +2,11 @@ class HowlersController < ApplicationController
   def index
     @howlers = current_user.howlers.order(:created_at).reverse_order
   end
+
+  def destroy
+    howler = Howler.find(params[:id])
+    howler.delete
+    flash[:notice] = "You have successfully deleted Howler"
+    redirect_to howlers_path
+  end
 end
