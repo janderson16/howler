3fd645357d8e43987bd51c99638452ec84b42b2f
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-05-31 16:05:21 -0600

    Cews edit howler (#13)
    * create show page
    
    * add edit to show page; make content and title clickable
    
    * make edit work as expected
    
    * blank graphs now display data w/ zero, added table to show
    
    * add table to show that updates

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tones, only: [:create, :index]
-      resources :howlers, only: [:create]
+      resources :howlers, only: [:create, :update]
     end
   end
 end
