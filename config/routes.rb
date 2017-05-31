96174ba52f4c661c7a362e9c182318468b9f0104
Max Glassie <msglassie@gmail.com>
2017-05-31 15:23:03 -0600

    creates a test for the extensions API

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tones, only: [:create, :index]
       resources :howlers, only: [:create]
+      resources :extensions, only: [:create]
     end
   end
 end
