3f5369d874859b4e29d47575791de9dadf22d65d
Charlotte Moore <mooc123@gmail.com>
2017-06-01 17:26:29 -0600

    fixed failing test

--- spec/features/user_can_see_howler_spec.rb
+++ spec/features/user_can_see_howler_spec.rb
@@ -7,7 +7,21 @@ describe "As a user" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-      howler = user_1.howlers.create(title: 'sample', text: 'sample one')
+      howler = user_1.howlers.create(title: 'sample',
+                                     text: 'sample one',
+                                     anger: 0.0,
+                                     disgust: 0.0,
+                                     fear: 0.0,
+                                     joy: 0.0,
+                                     sadness: 0.0,
+                                     analytical: 0.0,
+                                     confident: 0.0,
+                                     tentative: 0.0,
+                                     openness: 0.0,
+                                     conscientiousness: 0.0,
+                                     extraversion: 0.0,
+                                     agreeableness: 0.0,
+                                     emotional_range: 0.0)
 
       visit howler_path(howler)
 
